31f11353f8dabee531cb5ac67cd1309c55abd063
Jamie Greeff <madjam002@yahoo.co.uk>
2015-07-25 13:17:44 +0100

    new specular app class

--- index.js
+++ index.js
@@ -1,33 +1,7 @@
 import {Animation} from './src/animation'
 import {React} from './src/react'
 import {Beat} from './src/beat'
-import fs from 'fs'
-import path from 'path'
-
-require.uncache = (moduleName) => {
-  require.searchCache(moduleName, mod => {
-    console.log('unloaded', mod.id)
-    delete require.cache[mod.id]
-  })
-
-  Object.keys(module.constructor._pathCache).forEach(cacheKey => {
-    if (cacheKey.indexOf(moduleName) > 0) {
-      delete module.constructor._pathCache[cacheKey]
-    }
-  })
-}
-
-require.searchCache = (moduleName, callback) => {
-  let mod = require.resolve(moduleName)
-
-  if (mod && ((mod = require.cache[mod]) !== undefined)) {
-    (function run(mod) {
-      mod.children.forEach(child => run(child))
-
-      callback(mod)
-    })(mod)
-  }
-}
+import {EventEmitter} from 'events'
 
 export * from './src/fixture'
 export * from './src/component'
@@ -36,90 +10,61 @@ export * from './src/scene'
 export * from './src/create-fixture'
 export * from './src/animation'
 export * from './src/beat'
+export * from './src/watcher'
 
 let lastRender = Date.now()
 
-let rootElement = null
-
-let renderLoop = (callback) => {
-  if ((Date.now() - lastRender) < (1000 / 60)) {
-    setImmediate(renderLoop.bind(this, callback))
-    return
+export default class Specular extends EventEmitter {
+  use(func) {
+    func(this)
   }
 
-  let context = {
-    result: {}
+  start(path) {
+    const mainComponent = require(path)
+    this.rootPath = path
+    this.rootElement = React.createElement(mainComponent, {})
+
+    Beat.performBeat()
+
+    this.running = true
+    this._renderLoop()
   }
 
-  try {
-    React.render(rootElement, context)
-  } catch (ex) {
-    console.log('Render Exception', ex.stack || ex.message)
+  _renderLoop() {
+    this.render()
+
+    if (this.running)
+      setTimeout(this._renderLoop.bind(this), 1000 / 120)
   }
 
-  Beat.update(Date.now())
-
-  Animation.updateAll(Date.now())
-  lastRender = Date.now()
-
-  // ensure undefined values are 0
-  for (let i = 0; i <= 512; i++) {
-    if (!context.result[i]) context.result[i] = 0
-  }
-
-  callback(context.result)
-
-  setImmediate(renderLoop.bind(this, callback))
-}
-
-export default {
-  render(component) {
+  render() {
     let context = {
       result: {}
     }
 
-    component.context = context
-    render(component)
+    try {
+      React.render(this.rootElement, context)
+    } catch (ex) {
+      console.log('Render Exception', ex.stack || ex.message)
+    }
 
-    return context.result
-  },
+    Beat.update(Date.now())
 
-  start(element, callback) {
-    rootElement = element
-    renderLoop(callback)
-  },
+    Animation.updateAll(Date.now())
 
-  startWithWatch(componentPath, props, watchDir, callback) {
-    let mainComponent = require(componentPath)
-    rootElement = React.createElement(mainComponent, props)
+    // ensure undefined values are 0
+    for (let i = 0; i <= 512; i++) {
+      if (!context.result[i]) context.result[i] = 0
+    }
 
-    renderLoop(callback)
-
-    fs.watch(watchDir, {recursive: true}, e => {
-      // get state of components
-      let state = React.getTreeState() //NOT USED YET
-
-      console.log('Files changed, unloading components')
-
-      // start at root component and unload
-      require.uncache(path.resolve(componentPath))
-
-      // now load again
-      mainComponent = require(path.resolve(componentPath))
-
-      // for now just unmount all so that everything is remounted
-      // TODO think about keeping state in the future?
-      React.unmountAll()
-
-      rootElement = React.createElement(mainComponent, rootElement.props)
-    })
-  },
+    this.emit('output', [context.result]) // TODO support more than one universe
+  }
 
   setRootProps(props) {
-    Object.assign(rootElement.props, props)
-  },
+    Object.assign(this.rootElement.props, props)
+  }
 
   getRootProps() {
-    return rootElement.props
+    return this.rootElement.props
   }
 }
