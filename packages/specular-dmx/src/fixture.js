41c8d4ed3fbcd785a0335848b7d14571063270cc
Jamie Greeff <madjam002@yahoo.co.uk>
2015-10-30 23:38:53 +0000

    Render properly when react updates

--- packages/specular-dmx/src/fixture.js
+++ packages/specular-dmx/src/fixture.js
@@ -2,15 +2,19 @@ import React, {PropTypes} from 'react'
 
 export class Fixture extends React.Component {
 
+  static contextTypes = {
+    specularQueueUpdate: PropTypes.func.isRequired,
+  }
+
   construct(element) {
     this._currentElement = element
     this._channels = {}
     this._universe = null
   }
 
-  mountComponent(rootId, transaction) {
+  mountComponent(rootId, transaction, context) {
     const props = this._currentElement.props
-    this.update({}, props)
+    this.update(context, {}, props)
   }
 
   unmountComponent() {
@@ -19,11 +23,11 @@ export class Fixture extends React.Component {
 
   receiveComponent(nextElement, transaction, context) {
     const oldProps = this._currentElement.props
-    this.update(oldProps, nextElement.props)
+    this.update(context, oldProps, nextElement.props)
     this._currentElement = nextElement
   }
 
-  update(oldProps, props) {
+  update(context, oldProps, props) {
     this._channels = {}
     this._universe = props.universe
 
@@ -35,6 +39,8 @@ export class Fixture extends React.Component {
       const channelNumber = parseInt(index) + props.start
       this._channels[channelNumber] = channels[index]
     }
+
+    context.specularQueueUpdate()
   }
 
 }
--- src/renderer/render.js
+++ src/renderer/render.js
@@ -1,3 +1,5 @@
+import _ from 'lodash'
+import React, {PropTypes} from 'react'
 import ReactInstanceHandles from 'react/lib/ReactInstanceHandles'
 import ReactUpdates from 'react/lib/ReactUpdates'
 import ReactElement from 'react/lib/ReactElement'
@@ -15,9 +17,26 @@ export function render(element, renderPasses) {
     'render(): You must pass a valid ReactElement.',
   )
 
+  const wrapperComponent = React.createClass({
+    childContextTypes: {
+      specularQueueUpdate: PropTypes.func,
+    },
+    componentWillMount() {
+      this.specularQueueUpdate = _.debounce(() => renderComponents(this, renderPasses))
+    },
+    getChildContext() {
+      return {
+        specularQueueUpdate: this.specularQueueUpdate,
+      }
+    },
+    render() {
+      return element
+    },
+  })
+
   const id = ReactInstanceHandles.createReactRootID()
   const transaction = ReactUpdates.ReactReconcileTransaction.getPooled()
-  const component = instantiateReactComponent(element)
+  const component = instantiateReactComponent(React.createElement(wrapperComponent))
 
   transaction.perform(() =>
     component.mountComponent(id, transaction, {})
