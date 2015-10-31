aa3eb2aeeaeb3fd88c3c102ec2ab839c8235f4a6
Jamie Greeff <madjam002@yahoo.co.uk>
2015-10-31 11:18:32 +0000

    Use artnoot for artnet package as it's more reliable

--- packages/specular-dmx-artnet/src/index.js
+++ packages/specular-dmx-artnet/src/index.js
@@ -1,17 +1,20 @@
-import artnet from 'artnet'
+import artnoot from 'artnoot'
 
 export default function ArtNet(host) {
-  const conn = artnet({
+  const client = new artnoot.Client({
     host,
-  })
+  }).connect()
+
+  client.set(0, 1, 0) // initial packet
 
   return function (universeData) {
     const data = new Array(512)
 
-    for (const k in universeData) {
-      data[k] = universeData[k]
+    for (let i = 1; i <= 512; i++) {
+      data[i - 1] = Math.min(Math.round(universeData[i] || 0), 255)
     }
 
-    conn.set(data)
+    client.data[0] = data
+    client._send()
   }
 }
