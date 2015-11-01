653686be1059f790a61f5054e9e7699beafe5e2a
Jamie Greeff <madjam002@yahoo.co.uk>
2015-11-01 18:33:08 +0000

    Add specular-live-reload

---
+++ packages/specular-live-reload/src/index.js
@@ -0,0 +1,40 @@
+import fs from 'fs'
+import path from 'path'
+
+require.uncache = moduleName => {
+  require.searchCache(moduleName, mod => {
+    delete require.cache[mod.id]
+  })
+
+  Object.keys(module.constructor._pathCache).forEach(cacheKey => {
+    if (cacheKey.indexOf(moduleName) > 0) {
+      delete module.constructor._pathCache[cacheKey]
+    }
+  })
+}
+
+require.searchCache = (moduleName, callback) => {
+  let mod = require.resolve(moduleName)
+
+  if (mod && ((mod = require.cache[mod]) !== undefined)) {
+    (function run(mod) {
+      mod.children.forEach(child => run(child))
+
+      callback(mod)
+    })(mod)
+  }
+}
+
+export default function LiveReload(watchDir, modPath) {
+  require(path.resolve(modPath))
+
+  fs.watch(path.resolve(watchDir), { recursive: true }, e => {
+    console.log('Files changed, reloading...')
+
+    // start at root component and uncache
+    require.uncache(path.resolve(modPath))
+
+    // now load again
+    require(path.resolve(modPath))
+  })
+}
