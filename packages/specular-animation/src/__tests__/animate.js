684d2ac38737ebff1e2c4080366958834464a856
Jamie Greeff <madjam002@yahoo.co.uk>
2015-11-25 20:13:18 +0000

    Add tests for <Animate /> receiving new tweens via props

--- packages/specular-animation/src/__tests__/animate.js
+++ packages/specular-animation/src/__tests__/animate.js
@@ -21,19 +21,19 @@ let Animate
 
 describe.only('<Animate /> component', function () {
 
+  beforeEach(function () {
+    loopMock.registerComponent = sinon.spy()
+    loopMock.unregisterComponentSpy = sinon.spy()
+
+    beatEngineMock.beatBasedTweens = []
+
+    Animate = proxyquire('../animate', {
+      './loop': loopMock,
+      './beat-engine': { BeatEngine: beatEngineMock },
+    }).Animate
+  })
+
   describe('initial state', function () {
-
-    beforeEach(function () {
-      loopMock.registerComponent = sinon.spy()
-      loopMock.unregisterComponentSpy = sinon.spy()
-
-      beatEngineMock.beatBasedTweens = []
-
-      Animate = proxyquire('../animate', {
-        './loop': loopMock,
-        './beat-engine': { BeatEngine: beatEngineMock },
-      }).Animate
-    })
 
     describe('animation with duration', function () {
       beforeEach(function () {
@@ -163,6 +163,81 @@ describe.only('<Animate /> component', function () {
           yoyo: true,
           easing: 'easeInOut',
           beat: 4,
+          to: 255,
+          from: 0,
+        })
+      })
+    })
+
+    describe('animation with multiple tweens with inheriting props and from values', function () {
+      beforeEach(function () {
+        this.tree = shallowRender(
+          <Animate from={{x: 12}} values={{x: 50, y: { to: 255, beat: 4, yoyo: true }}} yoyo={false} duration={420} easing="easeInOut">
+            {() => {}}
+          </Animate>
+        )
+      })
+
+      it('should create the tweens', function () {
+        const instance = this.tree.getMountedInstance()
+
+        expect(instance.tweens.x).to.contain({
+          yoyo: false,
+          easing: 'easeInOut',
+          to: 50,
+          from: 12,
+          duration: 420,
+        })
+
+        expect(instance.tweens.y).to.contain({
+          yoyo: true,
+          easing: 'easeInOut',
+          beat: 4,
+          to: 255,
+          from: 0,
+        })
+      })
+    })
+
+  })
+
+  describe('receive props', function () {
+
+    describe('adding beat animation', function () {
+      beforeEach(function () {
+        let values = {val: { to: 50 }}
+
+        this.tree = shallowRender(
+          <Animate values={values}>
+            {() => {}}
+          </Animate>
+        )
+
+        values = {
+          ...values,
+          newBeat: { to: 255, beat: 2 },
+        }
+
+        this.tree.getMountedInstance().componentWillReceiveProps({ values })
+      })
+
+      it('should register the component with the loop', function () {
+        expect(loopMock.registerComponent).to.have.been.calledOnce
+      })
+
+      it('should register the new tween with the beat engine', function () {
+        const instance = this.tree.getMountedInstance()
+
+        expect(beatEngineMock.beatBasedTweens).to.eql([instance.tweens.newBeat])
+      })
+
+      it('should create the new tween filling in default props', function () {
+        const instance = this.tree.getMountedInstance()
+
+        expect(instance.tweens.newBeat).to.contain({
+          yoyo: true,
+          easing: 'linear',
+          beat: 2,
           to: 255,
           from: 0,
         })
